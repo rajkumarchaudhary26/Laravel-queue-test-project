af5815b270b0824be6495fce68f7cb6ee3ef64b5
Raj <raj.kumar.chaudhary@okicom.co.jp>
2025-10-29 10:07:01 +0900

    chore: extend Redis retry timeout for large zip jobs

--- phpapp/src/config/queue.php
+++ phpapp/src/config/queue.php
@@ -21,7 +21,7 @@ return [
             'driver' => 'redis',
             'connection' => 'default',
             'queue' => env('REDIS_QUEUE', 'default'),
-            'retry_after' => 90,
+            'retry_after' => 7200,
             'block_for' => null,
             'after_commit' => false,
         ],
